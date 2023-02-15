a8d29e2cc54fee4f400022011be901c05c2fbf10
jimmyarq <120688833+jimmyarq@users.noreply.github.com>
2023-02-15 17:06:52 -0600

    Add files via upload
    Archivos finales.

--- 1-main/app.js
+++ 1-main/app.js
@@ -1,1 +1,16 @@
-// Escribe tu código aquí.
+function bigWords(string, array) {
+  return array.filter((element) => element.length > string.length);
+}
+
+function printArray(array) {
+  const ul = document.getElementById("list");
+  for (let i = 0; i < array.length; i++) {
+    const li = document.createElement("li");
+    li.appendChild(document.createTextNode(array[i]));
+    ul.appendChild(li);
+  }
+}
+
+const myArray = ['matrix', 'trinity', 'morpheus', 'mouse', 'switch', 'oracle'];
+const resultArray = bigWords('net', myArray);
+printArray(resultArray);
